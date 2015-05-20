01cae856b939130ef16d079e887188fc8b74bab8
ditaoktaria <wotemingsheonyit@gmail.com>
2015-05-20 14:40:49 +0700

    sukses

--- get_locations.php
+++ get_locations.php
@@ -2,11 +2,12 @@
 require 'config.php';
      
     try {
+    	//$q = intval($_GET['q']);
      
         $db = new PDO($dsn, $username, $password);
         $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-         
-        $sth = $db->query("SELECT * FROM locations");
+         if($_GET['s'] == '') $sth = $db->query("SELECT * FROM locations ");
+        else $sth = $db->query("SELECT * FROM locations where name='".$_GET['s']."' ");
         $locations = $sth->fetchAll();
          
         echo json_encode( $locations );
